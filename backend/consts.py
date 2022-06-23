4d94458dcc6e3a339719334ae15d0512dfd7d6eb
sjhamilton <sjh.hamilton@gmail.com>
2022-06-23 11:46:08 -0300

    Update consts.py

--- backend/consts.py
+++ backend/consts.py
@@ -13,9 +13,9 @@ VQGAN_COMMIT_ID = "e93a26e7707683d349bf5d5c41c5b0ef69b677a9"
 
 
 # We can customize generation parameters (see https://huggingface.co/blog/how-to-generate)
-GEN_TOP_K = None
-GEN_TOP_P = None
-TEMPERATURE  = None
+GEN_TOP_K = 40
+GEN_TOP_P = 0.9
+TEMPERATURE  = 0.7
 COND_SCALE = 10.0
 
 class ModelSize(Enum):
